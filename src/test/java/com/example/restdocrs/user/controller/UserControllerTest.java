c1faeaca2a317aa8389dfe23d1a85ca530543e19
jhmoon <jhmoon@howser.co.kr>
2023-09-22 00:12:10 +0900

    test: 레스트독 문서 생성 초안 작성

--- src/test/java/com/example/restdocrs/user/controller/UserControllerTest.java
+++ src/test/java/com/example/restdocrs/user/controller/UserControllerTest.java
@@ -2,14 +2,25 @@ package com.example.restdocrs.user.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc // -> webAppContextSetup(webApplicationContext)
 @AutoConfigureRestDocs // -> apply(documentationConfiguration(restDocumentation))
 @SpringBootTest // @WebMvcTest(NaverOrderController.class) 사용하여도 무관
 class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
 
+    @Test
+    @DisplayName("restDocs 문서")
+    void getUserInfoList() {
+        //문서 작성
+    }
 }
